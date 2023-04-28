314fc0ad5462ae17d1ae80388e41fd32f2ab3d97
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2023-04-28 17:09:58 +0200

    do not use deprecated environments for testing
    - comply with LE 11.1
    - make use of variables

--- ui-path-connector-test/src/com/axonivy/connector/uipath/test/UiPathMock.java
+++ ui-path-connector-test/src/com/axonivy/connector/uipath/test/UiPathMock.java
@@ -19,48 +19,52 @@ import org.apache.commons.lang3.StringUtils;
 import io.swagger.v3.oas.annotations.Hidden;
 
 /**
- * Mocks parts of the UI Path RPA API which has been used in the demo: <code>uiPathDemo/processes/uiPathRpa</code>
- * <p>docs<br/>https://platform.uipath.com/AXONPRESALES/AXONPRESALES/swagger/ui</p>
+ * Mocks parts of the UI Path RPA API which has been used in the demo:
+ * <code>uiPathDemo/processes/uiPathRpa</code>
+ * <p>
+ * docs<br/>
+ * https://platform.uipath.com/AXONPRESALES/AXONPRESALES/swagger/ui
+ * </p>
  */
-@Path("rpaMock")
+@Path(UiPathMock.PATH_SUFFIX)
 @PermitAll
 @Hidden
-public class UiPathMock
-{
+public class UiPathMock {
+
+  static final String PATH_SUFFIX = "rpaMock";
+  // URI where this mock can be reached: to be referenced in tests that use it!
+  public static final String URI = "{ivy.app.baseurl}/api/" + PATH_SUFFIX;
+
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   @Path("odata/Settings/UiPath.Server.Configuration.OData.GetLicense")
-  public String getLicense()
-  {
+  public String getLicense() {
     return load("json/license.json");
   }
 
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   @Path("odata/Jobs")
-  public String getJobs()
-  {
+  public String getJobs() {
     String template = load("json/jobs.json");
     String json = StringUtils.replace(template,
-            "Key\": \"string\",", "Key\": \""+UUID.randomUUID().toString()+"\",");
+            "Key\": \"string\",", "Key\": \"" + UUID.randomUUID().toString() + "\",");
     json = StringUtils.replace(json,
-            "PersistenceId\": \"string\",", "PersistenceId\": \""+UUID.randomUUID().toString()+"\",");
+            "PersistenceId\": \"string\",", "PersistenceId\": \"" + UUID.randomUUID().toString() + "\",");
     return json;
   }
 
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   @Path("odata/Robots")
-  public String getRobots()
-  {
+  public String getRobots() {
     return load("json/robots.json");
   }
 
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   @Path("odata/Releases")
-  public String getReleases()
-  {
+  public String getReleases() {
     return load("json/releases.json");
   }
 
@@ -68,9 +72,8 @@ public class UiPathMock
   @Produces(MediaType.APPLICATION_JSON)
   @Consumes(MediaType.APPLICATION_JSON)
   @Path("oauth/token")
-  public String getToken(String tokenRequest)
-  {
-    System.out.println("simulating request for token "+tokenRequest);
+  public String getToken(String tokenRequest) {
+    System.out.println("simulating request for token " + tokenRequest);
     return load("json/token.json");
   }
 
@@ -78,21 +81,16 @@ public class UiPathMock
   @Produces(MediaType.APPLICATION_JSON)
   @Consumes(MediaType.APPLICATION_JSON)
   @Path("odata/Jobs/UiPath.Server.Configuration.OData.StartJobs")
-  public String startJob(String tokenRequest)
-  {
-    System.out.println("simulating request for token "+tokenRequest);
+  public String startJob(String tokenRequest) {
+    System.out.println("simulating request for token " + tokenRequest);
     return load("json/startJob.json");
   }
 
-  private static String load(String path)
-  {
-    try(InputStream is = UiPathMock.class.getResourceAsStream(path))
-    {
+  private static String load(String path) {
+    try (InputStream is = UiPathMock.class.getResourceAsStream(path)) {
       return IOUtils.toString(is, StandardCharsets.UTF_8);
-    }
-    catch (IOException ex)
-    {
-      throw new RuntimeException("Failed to read resource: "+path);
+    } catch (IOException ex) {
+      throw new RuntimeException("Failed to read resource: " + path);
     }
   }
 }
