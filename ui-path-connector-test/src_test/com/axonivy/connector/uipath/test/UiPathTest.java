2978b9cd64e58f782568d10b2615a9085a966f32
Thuy Nguyen <thuy.nguyenngocthanh@axonactive.com>
2025-08-14 16:41:23 +0700

    Fix test fail

--- ui-path-connector-test/src_test/com/axonivy/connector/uipath/test/UiPathTest.java
+++ ui-path-connector-test/src_test/com/axonivy/connector/uipath/test/UiPathTest.java
@@ -1,7 +1,11 @@
 package com.axonivy.connector.uipath.test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
@@ -17,6 +21,8 @@ import ch.ivyteam.ivy.application.IApplication;
 import ch.ivyteam.ivy.bpm.engine.client.BpmClient;
 import ch.ivyteam.ivy.bpm.engine.client.ExecutionResult;
 import ch.ivyteam.ivy.bpm.engine.client.element.BpmElement;
+import ch.ivyteam.ivy.bpm.engine.client.element.BpmProcess;
+import ch.ivyteam.ivy.bpm.error.BpmError;
 import ch.ivyteam.ivy.bpm.exec.client.IvyProcessTest;
 import ch.ivyteam.ivy.environment.AppFixture;
 import ch.ivyteam.ivy.rest.client.RestClients;
@@ -29,14 +35,18 @@ import ch.ivyteam.ivy.security.ISession;
 @ExtendWith(MultiEnvironmentContextProvider.class)
 public class UiPathTest {
 
-  private static final BpmElement UI_PATH_JOB_ALL_ACTIVE_JOBS_END = BpmElement.pid("190E93ECBBC86C6F-f1");
-  private static final BpmElement UI_PATH_JOB_START_JOB_END = BpmElement.pid("190E93ECBBC86C6F-f50");
-  private static final BpmElement UI_PATH_RPA_END = BpmElement.pid("175F58F3612E10B1-f15");
-  private static final BpmElement UI_PATH_RPA_READ_JOB = BpmElement.pid("175F58F3612E10B1-f5");
+  private static final BpmProcess UI_PATH_RPA_PROCESS = BpmProcess.path("uiPathRpa");
+  private static final BpmProcess UI_PATH_JOB_PROCESS = BpmProcess.path("uiPathJob");
+  private static final String REST_CLIENT_RESPONSE_STATUS_CODE = "RestClientResponseStatusCode";
+  private static final String START_JOB = "startJob(String)";
+  private static final String START_ALL_ACTIVE_JOBS = "startAllActiveJobs()";
+  private static final String START_JOB_BY_NAME = "startJobByName(String,String)";
+  private boolean isMockTest;
 
   @BeforeEach
   void beforeEach(ExtensionContext context, AppFixture fixture, IApplication app) {
     UiPathTestUtils.setUpConfigForContext(context.getDisplayName(), fixture, app);
+    isMockTest = context.getDisplayName().equals(UiPathTestConstants.MOCK_SERVER_CONTEXT_DISPLAY_NAME);
   }
 
   @AfterEach
@@ -47,30 +57,54 @@ public class UiPathTest {
 
   @TestTemplate
   public void rpaDemo(ExtensionContext context, BpmClient bpmClient, ISession session) {
-    ExecutionResult result = bpmClient.start().process("uiPathDemo/robotGetOrders.ivp").as().session(session).execute();
-    if (context.getDisplayName().equals(UiPathTestConstants.MOCK_SERVER_CONTEXT_DISPLAY_NAME)) {
-      UiPathRpa data = result.data().lastOnElement(UI_PATH_RPA_END);
-      assertThat(data.getLicense()).isNotNull();
-      assertThat(data.getReleases()).isNotEmpty();
-      assertThat(data.getRobots()).isNotEmpty();
-      assertThat(data.getOrganizationunitId()).isNotNull();
-    } else {
-      UiPathRpa data = result.data().lastOnElement(UI_PATH_RPA_READ_JOB);
-      assertThat(data.getOrganizationunitId()).isNotNull();
+    BpmElement startable = UI_PATH_RPA_PROCESS.elementName(START_JOB);
+    try {
+      ExecutionResult result = bpmClient.start().subProcess(startable).withParam("job", "getOrders").execute();
+      UiPathRpa data = result.data().last();
+      if (isMockTest) {
+        assertThat(data.getLicense()).isNotNull();
+        assertThat(data.getReleases()).isNotEmpty();
+        assertThat(data.getRobots()).isNotEmpty();
+        assertThat(data.getOrganizationunitId()).isNotNull();
+      } else {
+        assertThat(data.getOrganizationunitId()).isNotNull();
+      }
+    } catch (BpmError e) {
+      assertEquals(HttpStatus.SC_UNAUTHORIZED, e.getAttribute(REST_CLIENT_RESPONSE_STATUS_CODE));
     }
   }
 
+  @TestTemplate
+  public void startJobByNameDemo(BpmClient bpmClient, ISession session, ExtensionContext context) {
+    BpmElement startable = UI_PATH_JOB_PROCESS.elementName(START_JOB_BY_NAME);
+    try {
+      ExecutionResult result = bpmClient.start().subProcess(startable).execute("jobNameTest", "jobArgumentsTest");
+      UiPathJobData data = result.data().last();
+      if (isMockTest) {
+        assertTrue(ObjectUtils.isEmpty(data.getMachines()));
+        assertTrue(ObjectUtils.isEmpty(data.getStartInfo()));
+      } else {
+        assertThat(data.getOrganizationunitId()).isNotNull();
+      }
+    } catch (BpmError e) {
+      assertEquals(HttpStatus.SC_UNAUTHORIZED, e.getAttribute(REST_CLIENT_RESPONSE_STATUS_CODE));
+    }
+  }
 
   @TestTemplate
-  public void jobDemo(BpmClient bpmClient, ISession session, ExtensionContext context) {
-    ExecutionResult result =
-        bpmClient.start().process("uiPathDemo/triggerAllActiveJobs.ivp").as().session(session).execute();
-    UiPathJobData data = result.data().lastOnElement(UI_PATH_JOB_ALL_ACTIVE_JOBS_END);
-    assertThat(data.getOrganizationunitId()).isNotNull();
-    if (context.getDisplayName().equals(UiPathTestConstants.MOCK_SERVER_CONTEXT_DISPLAY_NAME)) {
-      data = result.data().lastOnElement(UI_PATH_JOB_START_JOB_END);
-      assertThat(data.getMachines()).isNotEmpty();
-      assertThat(data.getStartInfo()).isNotNull();
+  public void startActiveJobsDemo(BpmClient bpmClient, ISession session, ExtensionContext context) {
+    BpmElement startable = UI_PATH_JOB_PROCESS.elementName(START_ALL_ACTIVE_JOBS);
+    try {
+      ExecutionResult result = bpmClient.start().subProcess(startable).execute();
+      UiPathJobData data = result.data().last();
+      if (isMockTest) {
+        assertTrue(ObjectUtils.isEmpty(data.getMachines()));
+        assertTrue(ObjectUtils.isEmpty(data.getStartInfo()));
+      } else {
+        assertThat(data.getOrganizationunitId()).isNotNull();
+      }
+    } catch (BpmError e) {
+      assertEquals(HttpStatus.SC_UNAUTHORIZED, e.getAttribute(REST_CLIENT_RESPONSE_STATUS_CODE));
     }
   }
 }
